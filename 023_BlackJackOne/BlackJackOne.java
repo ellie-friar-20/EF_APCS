104cb2e8fa7280e2342da0e3ab9400ae34f9de09
Eleanor Friar <ef1610079@027Labs-iMac.local>
2019-12-13 10:22:27 -0600

    blackjack not complete

--- 023_BlackJackOne/BlackJackOne.java
+++ 023_BlackJackOne/BlackJackOne.java
@@ -3,5 +3,14 @@ public class BlackJackOne
     public static void main(String[] args)
     {
         
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Do you want to hit or stay?");
+        int value = newvalue+;
+        if (newvalue > 21)
+        {
+
+        }
+    
+        
     }
 }
--- 024_ShippingCalculator/ShippingCalculator.java
+++ 024_ShippingCalculator/ShippingCalculator.java
@@ -32,5 +32,6 @@ public class ShippingCalculator
         {
             System.out.println("This package cannot be shipped.");
         }
+        scan.close();
     }
 }
