13c16e6a2f871c9705b5205e2581bd5b787382b3
Eleanor Friar <ef1610079@003Labs-iMac.local>
2020-03-02 15:59:50 -0600

    changes

--- 035_DateTranslator/DateTranslator.java
+++ 035_DateTranslator/DateTranslator.java
@@ -1,9 +1,34 @@
 import java.util.Scanner;
 public class DateTranslator {
-    public static void userInput() {
+    /* takes the user input and decides which format it follows and then calls open method to print it out*/
+    public static void main(String[] args) {
         String userInput;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please input the month, the day, and the year you want: ");
-        userInput = scan.nextLine();
+        System.out.println("Please input the month: ");
+        int month = scan.nextInt();
+        System.out.println("Please input the day: ");
+        int day = scan.nextInt();
+        System.out.println("Please input the year: ");
+        int year = scan.nextInt();
+        System.out.println("Choose whether you want the American or European date format: ");
+        String whichFormat = scan.next();
+        if(whichFormat.equals("American"))
+        {
+            dateAmerican(month, day, year);
+        } else if(whichFormat.equals("European"))
+        {
+            dateEuropean(day, month, year);
+        } else
+        {
+            System.out.println("You need to start over incorrect input");
+        }
     }
+public static void dateAmerican(int month, int day, int year)
+{
+    System.out.println("Date:" + month + "/" + day + "/" + year);
+}
+public static void dateEuropean(int day, int month, int year)
+{
+    System.out.println("Date:" + day + "/" + month + "/" + year);
+}
 }
