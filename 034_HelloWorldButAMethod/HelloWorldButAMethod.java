314740ec86152c9c7e5ca218532c347cd6fb6d56
Eleanor Friar <ef1610079@027Labs-iMac.local>
2020-03-02 10:01:40 -0600

    homework

---
+++ 034_HelloWorldButAMethod/HelloWorldButAMethod.java
@@ -0,0 +1,14 @@
+public class HelloWorldButAMethod{
+    /*created a custom method that prints out hello world*/ 
+    public static void helloWorld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args)
+    {
+        helloWorld();
+    }
+}
+
+
+
+
