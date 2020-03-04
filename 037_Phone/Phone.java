302e59c67b5ac1956cd31c952b78efc440a6af83
Eleanor Friar <ef1610079@003Labs-iMac.local>
2020-03-04 14:09:31 -0600

    more

--- 037_Phone/Phone.java
+++ 037_Phone/Phone.java
@@ -1,1 +1,15 @@
-public
+public class Phone {
+    private String version;
+    private String color;
+    private int batteryLife;
+    private int ageOfPhone;
+    public Phone(String phoneVersion, String phoneColor, int phoneBatteryLife, int phoneAgeOfPhone) {
+        version = phoneVersion;
+        color = phoneColor;
+        batteryLife = phoneBatteryLife;
+        ageOfPhone = phoneAgeOfPhone;
+    }
+    public 
+
+
+}
